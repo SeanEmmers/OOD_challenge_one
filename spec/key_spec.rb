0165e4c673f0155237f1044c9521740af49abc8b
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-06-02 20:37:58 +0100

    refactoring tests

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -4,14 +4,14 @@ describe Key do
 
   describe '#lock' do
     it 'locks the diary' do
-      expect(subject.locked).to eq true
+      expect(subject.locked?).to eq true
     end
   end
 
   describe '#unlock' do
     it 'unlocks the diary' do
       subject.unlock
-      expect(subject.locked).to eq false
+      expect(subject.locked?).to eq false
     end
   end
 end
